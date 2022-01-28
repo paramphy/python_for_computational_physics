337759c20e4124ca33364a4eb1783b8a12e38fd5
Paramesh Chandra <parameshchandra28@gmail.com>
2022-01-28 12:23:09 +0530

    Update Chapt7exmpl01.py

--- Chapter 7/Chapt7exmpl01.py
+++ Chapter 7/Chapt7exmpl01.py
@@ -15,14 +15,15 @@ for i in range(1, N):
     else:
         x[i] = x[i - 1] + 1
 
-RMS = np.array([sqrt(i * i) for i in x])
+#RMS = np.array([sqrt(i * i) for i in x])
 
 # syntax for 3-D projection
 fig, ax = plt.subplots()  # Create a figure and an axes.
 ax.plot(t, x, label="Steps")  # Plot some data on the axes.
-ax.plot(t, RMS, label="RMS")  # Plot more data on the axes...
+#ax.plot(t, RMS, label="RMS")  # Plot more data on the axes...
 ax.set_xlabel("t")  # Add an x-label to the axes.
 ax.set_ylabel("x")  # Add a y-label to the axes.
 ax.set_title("Chapter 7 example 01")  # Add a title to the axes.
 ax.legend()  # Add a legend.
+ax.vlines(5,-50,10)
 plt.show()
